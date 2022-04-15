5f6b33b2ac1688333585a8c4f50724f8b740eae6
bb8dd <ldh19971027@gmail.com>
2022-04-15 23:49:03 +0900

    220415 2.3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const a = 5;
+const a = 5; // ! 상수 ( Constant ) 재할당 불가능
 const b = 2;
-let myName = "LeeDongHyeon";
+let myName = "LeeDongHyeon"; // ! 재할당 가능
 
 console.log(a + b);
 console.log(a - b);

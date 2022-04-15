e5ec80bb354ce5892547a29d455bc74aa6e9648e
realludaga <ldh19971027@gmail.com>
2022-04-15 23:27:09 +0900

    2.3 const + let

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("hi");
+const a = 5;
+const b = 2;
+let myName = "LeeDongHyeon";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.log("Hello " + myName);
+
+myName = "DongHyeon";
+
+console.log("Your name is " + myName);

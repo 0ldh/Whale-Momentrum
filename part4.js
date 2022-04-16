62dd62e949e4b89858ce771cdb123c745ebb30ad
bb8dd <ldh19971027@gmail.com>
2022-04-17 02:33:49 +0900

    Momentum Update

---
+++ part4.js
@@ -0,0 +1,12 @@
+// 객체생성
+
+const me = {
+   firstName : "Lee",
+   name : "DongHyeon",
+   weight : "72kg",
+   height : "177cm"
+}
+
+console.log(me);
+console.log(me.firstName);
+console.log(me[0]);

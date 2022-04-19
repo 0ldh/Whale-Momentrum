af9699684f0c1f00f0a4a5dfcbce7f3b5f77dca5
bb8dd <ldh19971027@gmail.com>
2022-04-19 20:46:27 +0900

    function part1

--- part5.js
+++ part5.js
@@ -4,12 +4,22 @@ const cal = (a,b,c)=>{
    if (b === "*") return a * b;
    if (b === "/") return a / b;
    else alert("연산자를 올바르게 입력해주세요");
-}
+};
 
 const clog = () => {
    return console.log(3);
 }
 
 
-console.log(cal(3,4));
 clog()
+
+// ------------------------ 강의 
+
+function sayHello(name) {
+   console.log("Hello" + name);
+}
+
+sayHello("a");
+sayHello("b");
+sayHello("c");
+sayHello("D");

74ab4f55bb576e718b3f1772d13af63c555e71c2
bb8dd <ldh19971027@gmail.com>
2022-04-22 21:03:06 +0900

    commit

--- part5.js
+++ part5.js
@@ -1,8 +1,8 @@
 const cal = (a,b,c)=>{
-   if (b === "+") return a + b;
-   if (b === "-") return a - b;
-   if (b === "*") return a * b;
-   if (b === "/") return a / b;
+   if (b == "+") return console.log(a + c);
+   if (b == "-") return console.log(a - c);
+   if (b == "*") return console.log(a * c);
+   if (b == "/") return console.log(a / c);
    else alert("연산자를 올바르게 입력해주세요");
 };
 
@@ -15,11 +15,26 @@ clog()
 
 // ------------------------ 강의 
 
-function sayHello(name) {
-   console.log("Hello" + name);
+// function sayHello(name, age) {
+//    console.log("Hello " + name + "Age is " + age);
+//    return 111;
+// }
+
+// sayHello("a", 13);
+// sayHello("b", 15);
+// sayHello("c", 22);
+// sayHello("D", 256);
+// console.log(sayHello("a",30))
+
+cal(100, "-", 50);
+
+
+const player = {
+   name : "ldH",
+   sayHello : (name) => {
+      console.log("Hello " + name);
+   }
 }
 
-sayHello("a");
-sayHello("b");
-sayHello("c");
-sayHello("D");
+console.log(player["name"])
+player.sayHello("nico")

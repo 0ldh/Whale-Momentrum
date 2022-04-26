91481f297936778c4d996c9c2db70ff350c83d2d
bb8dd <ldh19971027@gmail.com>
2022-04-26 22:53:49 +0900

    cal + return

--- part7.js
+++ part7.js
@@ -3,4 +3,33 @@ const calculate = (a) => {
    return a + 2;
 }
 
-alert(calculate(age));
+const cal = {
+   plus : (a,b) =>{
+      return a + b
+   },
+   minus : (a,b) =>{
+      return a - b;
+   },
+   mul : (a,b) => {
+      return a*b;
+   },
+   square : (a,b) => {
+      return a**b;
+   },
+   division: (a,b) => {
+      return a / b;
+   }
+}
+
+let res = [];
+
+const plusRes = cal.plus(6,3);
+const minusRes = cal.minus(plusRes, 3);
+const squaerRes = cal.minus(minusRes, 3);
+const mulRes = cal.minus(squaerRes, 3);
+const diviRes = cal.minus(mulRes, 3);
+
+res.push(plusRes,minusRes,squaerRes,squaerRes,mulRes,diviRes);
+
+console.log(res)
+

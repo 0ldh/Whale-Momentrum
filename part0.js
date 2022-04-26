c9fe1719e9dc718ba008f6726fbbd5dc33c8bb64
bb8dd <ldh19971027@gmail.com>
2022-04-26 22:56:42 +0900

    arrangement

---
+++ part0.js
@@ -0,0 +1,22 @@
+// Boolean
+// ? True or False
+const amIFat = false;
+console.log(amIFat);
+
+// null
+// ? 아무것도 아닌 값
+const a = null;
+console.log(a);
+
+// undefind 
+// ? 값이 할당되지 않음
+let b;
+console.log(b);
+
+// Referance Error
+// ? 변수가 선언되지 않음
+if (true) {
+   const c = 3;
+}
+console.log(c);
+// * 스코프 개념

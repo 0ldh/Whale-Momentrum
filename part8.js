58a9a2c2e7a6e09ab1ff4a2b7d5de0d2e5d17e8d
bb8dd <ldh19971027@gmail.com>
2022-04-26 23:47:35 +0900

    prompt

---
+++ part8.js
@@ -0,0 +1,12 @@
+ let age = prompt("How old are you?")
+
+ age = parseInt(age);
+age2 =  Number(age);
+
+ console.log(age);
+ console.log(age2);
+
+
+let phoneNumber = parseInt( prompt("What's your phone number?"))
+
+console.log(phoneNumber);

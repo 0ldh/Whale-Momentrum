9134f820d2dace7d02e7fc27323ce14687b5359f
bb8dd <ldh19971027@gmail.com>
2022-04-29 22:03:02 +0900

    css update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const h1 = document.querySelector(".hello h1");
 
 
 const  handleTitleClick = () =>{
-   if(h1.style.color == "blue") h1.style.color = "tomato";
+   if(h1.style.color === "blue") h1.style.color = "tomato";
    else h1.style.color = "blue";
    console.log("title was clicked");
 }

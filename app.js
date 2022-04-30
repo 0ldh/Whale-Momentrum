faf160562c0910b8047c3370ee69e63a18a2dc1d
bb8dd <ldh19971027@gmail.com>
2022-04-30 22:18:00 +0900

    Class List toggle

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const h1 = document.querySelector(".hello h1");
 
 
 const  handleTitleClick = () =>{
-   const clickedClass = "clicked"
+   // const clickedClass = "clicked"
 
-   if (h1.classList.contains(clickedClass)) h1.classList.remove(clickedClass);
-   else h1.classList.add(clickedClass);
+   // if (h1.classList.contains(clickedClass)) h1.classList.remove(clickedClass);
+   // else h1.classList.add(clickedClass);
+
+   h1.classList.toggle("clicked");
 }
 
 

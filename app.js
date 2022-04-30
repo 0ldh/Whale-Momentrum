a44aa9dd77479e8549a2ea914b5cddd8fdb7a238
bb8dd <ldh19971027@gmail.com>
2022-04-30 22:08:47 +0900

    classList

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const h1 = document.querySelector(".hello h1");
 
 const  handleTitleClick = () =>{
    const clickedClass = "clicked"
-   if (h1.className === "clicked") h1.className ="";
-   else h1.className = "clicked";
+
+   if (h1.classList.contains(clickedClass)) h1.classList.remove(clickedClass);
+   else h1.classList.add(clickedClass);
 }
 
 

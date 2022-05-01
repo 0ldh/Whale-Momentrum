8bbe6dec46a73ee55a93443ef042c8df344027fb
bb8dd <ldh19971027@gmail.com>
2022-05-01 20:46:56 +0900

    EVENT 제어

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("#login-form input");
 
-const onLoginBtnClick = () => {
+const onLoginSubmit = (event) => {
+   event.preventDefault();
    const userName = loginInput.value;
 
    console.log(userName);
 }
 
-// loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit)
+
+// preventDefault() =>  기본동작을 막아줌
+// addEventListener => 이벤트를 매개변수로 함수를 실행한다
+

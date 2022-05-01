78d12dbdf0d01380962b912e37dc1ceca24f1ae8
bb8dd <ldh19971027@gmail.com>
2022-05-01 23:23:15 +0900

    Event argument

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
 
+const link = document.querySelector("a")
+
 const onLoginSubmit = (event) => {
    event.preventDefault();
    const userName = loginInput.value;
@@ -8,7 +10,16 @@ const onLoginSubmit = (event) => {
    console.log(userName);
 }
 
+const handleLinkClick = (event) => {
+   event.preventDefault();
+   console.dir(event);
+   alert("Click!");
+}
+
+
 loginForm.addEventListener("submit", onLoginSubmit)
+link.addEventListener("click", handleLinkClick)
+
 
 // preventDefault() =>  기본동작을 막아줌
 // addEventListener => 이벤트를 매개변수로 함수를 실행한다

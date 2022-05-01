261300b0cd4635fa92e58c57439a05f85d040977
bb8dd <ldh19971027@gmail.com>
2022-05-01 19:40:16 +0900

    form submit

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const loginButton = loginForm.querySelector("button");
 const onLoginBtnClick = () => {
    const userName = loginInput.value;
 
-   if (userName==="") alert("이름을 입력해 주세요");
-   else if (userName.length > 15) alert("15글자를 초과한 이름입니다");
-   
+   console.log(userName);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+// loginButton.addEventListener("click", onLoginBtnClick);

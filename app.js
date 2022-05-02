8a1efb1e8c0d3dd385986f7153485aace5f71c6a
bb8dd <ldh19971027@gmail.com>
2022-05-02 21:25:23 +0900

    주석 추가

--- app.js
+++ app.js
@@ -10,30 +10,31 @@ const USERNAME_KEY = "username";
 
 // ! 함수
 const paintGreeting = (username) => {
-   greeting.classList.remove(HIDDEN_CLASSNAME);
-   greeting.innerText = `Hello ${username}`;
+   greeting.classList.remove(HIDDEN_CLASSNAME); // todo username을 매개변수로 .hidden 제거
+   greeting.innerText = `Hello ${username}`; // todo #greeting에 username text로 입력
 }
 
 const onLoginSubmit = (event) => {
-   event.preventDefault();
-   loginForm.classList.add(HIDDEN_CLASSNAME);
-   const userName = loginInput.value;
+   event.preventDefault(); // todo submit 이벤트의 기본행동을 멈춤 (refresh)
+   loginForm.classList.add(HIDDEN_CLASSNAME); // todo username을 입력하면 form에 .hidden 추가 후 숨김
+   const userName = loginInput.value; // todo user가 입력한 username 변수에 저장
 
-   localStorage.setItem(USERNAME_KEY, userName);
+   localStorage.setItem(USERNAME_KEY, userName); // todo username locastorage에 저장
    
-   paintGreeting(userName);
+   paintGreeting(userName); // todo 입력한 username을 매개변수로 paint greeting 함수 실행
 
-   console.log(userName);
+   console.log(userName); //
 }
 
 // ********************************************************
-const savedUsername = localStorage.getItem(USERNAME_KEY);
+const savedUsername = localStorage.getItem(USERNAME_KEY); // todo localstorage에 있는 username을 키로 value 값 호출
 
 if (savedUsername === null) {
-   // USER NAME이 저장되어 있는 경우
+   // todo usernmae이 비어있는 경우 
    loginForm.classList.remove(HIDDEN_CLASSNAME);
    loginForm.addEventListener("submit", onLoginSubmit)
 } else {
+   // todo username이 입력되어 있는 경우
    paintGreeting(savedUsername);
 }
 

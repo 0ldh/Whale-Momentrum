ab392b4dd24c251180bddf0c9ec9ac6df0cf70e0
bb8dd <ldh19971027@gmail.com>
2022-05-02 19:24:45 +0900

    paintGReeting, localStorage

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
+// ! 변수
 const greeting = document.querySelector("#greeting")
 const link = document.querySelector("a")
-const HIDDEN_CLASSNAME = "hidden"
 const body = document.querySelector("body");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+// ! 함수
+const paintGreeting = (username) => {
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+   greeting.innerText = `Hello ${username}`;
+}
 
 const onLoginSubmit = (event) => {
    event.preventDefault();
    loginForm.classList.add(HIDDEN_CLASSNAME);
    const userName = loginInput.value;
 
-   localStorage.setItem("username", userName);
-
-   greeting.innerText = `Hello ${userName}`;
-   greeting.classList.remove(HIDDEN_CLASSNAME);
+   localStorage.setItem(USERNAME_KEY, userName);
+   
+   paintGreeting(userName);
 
    console.log(userName);
 }
 
-const onLoadBody = () => {
-   if (localStorage.getItem("username")) {
-      const userName = localStorage.getItem("username"); 
-      greeting.innerText = `Hello ${userName}`;
-      loginForm.classList.add(HIDDEN_CLASSNAME);
-      greeting.classList.remove(HIDDEN_CLASSNAME);
+// ********************************************************
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-      console.log("LOAD")
-   } 
+if (savedUsername === null) {
+   // USER NAME이 저장되어 있는 경우
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit", onLoginSubmit)
+} else {
+   paintGreeting(savedUsername);
 }
-
-
-body.addEventListener("load", onLoadBody)
-loginForm.addEventListener("submit", onLoginSubmit)
-
 
 // preventDefault() =>  기본동작을 막아줌
 // addEventListener => 이벤트를 매개변수로 함수를 실행한다

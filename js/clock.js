3531dc85cf84928c762fe335298d70dfa2b1c018
bb8dd <ldh19971027@gmail.com>
2022-05-04 20:31:41 +0900

    시 계

--- js/clock.js
+++ js/clock.js
@@ -3,9 +3,7 @@ const clock = document.querySelector("#clock");
 const getClock = () => {
    const date = new Date();
    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-   console.log
 
-   date.getFullYear();
 }
 
 

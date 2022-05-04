ce35eab76f76fab7ceedefbaeaa1484e66fe0755
bb8dd <ldh19971027@gmail.com>
2022-05-04 22:58:27 +0900

    CLOCK 00 PADSTART

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("#clock");
 
 const getClock = () => {
    const date = new Date();
-   clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+   clock.innerText = `${date.getHours()}:${date.getMinutes()}:${String(date.getSeconds()).padStart(2,"0")}`
 
 }
 

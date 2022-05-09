a8c7cfca28530d3be3aa146d7465d88c4e71265e
bb8dd <ldh19971027@gmail.com>
2022-05-09 20:44:02 +0900

    todo.js => li add

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,21 @@ const todoForm = document.getElementById("todo-form");
 const toDoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const patinTodo = (newTodo) =>{
+   const li = document.createElement("li");
+   const span = document.createElement("span");
+   li.appendChild(span);
+   span.innerText = newTodo;
+   todoList.appendChild(li);
+} 
+
 const handleToDoSubmit = (event) => {
    event.preventDefault();
    const newTodo = toDoInput.value;
    toDoInput.value = "";
+   patinTodo(newTodo);
 }
+
+
 
 todoForm.addEventListener("submit", handleToDoSubmit)

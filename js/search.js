8b4f1c818d4e2a8aba959dc46757f68fc030d296
ldH <ldh19971027@gmail.com>
2022-05-17 16:03:06 +0900

    구글검색

--- js/search.js
+++ js/search.js
@@ -1,15 +1,23 @@
 const searchBox = document.querySelector(".searchbox");
 const search = searchBox.querySelector("input");
 
-console.log(search);
-console.log(searchBox);
-
-const handleOp = () => {
+const handleFocus = () => {
     searchBox.style.opacity = "1";
+};
+const handleFocusOut = ()=> {
+    searchBox.style.opacity = "0.5";
+};
+const doSearch = (event) => {
+    if (event.keyCode === 13) {
+        const val = search.value;
+        console.log(val)
+        location.href = `https://google.com/search?q=${val}`
+    }
 }
 
+search.addEventListener("focus", handleFocus);
+search.addEventListener("focusout", handleFocusOut);
 
-search.addEventListener("focus", handleOp)
-search.addEventListener("focusout", ()=> {
-    searchBox.style.opacity = "0.5";
-})
+// google.com/search?q= + value값
+
+search.addEventListener("keyup", doSearch);

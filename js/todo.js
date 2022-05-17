7fe5b91142ae92342f1dfd16533e4b5ae266937d
bb8dd <ldh19971027@gmail.com>
2022-05-17 22:45:16 +0900

    Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,17 @@
 const todoForm = document.getElementById("todo-form"); // todo 할 일 작성 폼 선택
 const toDoInput = todoForm.querySelector("input"); // todo 할 일 입력창 선택
 const todoList = document.getElementById("todo-list"); // todo 할 일 목록 선ㅌ택
+const todos = [];
 
 // * 할 일 목록에 할 일 추가 함수
 const deleteTodo = (event) => {
    const li = event.target.parentElement;
    li.remove();
 }
+
+const saveToDos = () => {
+   localStorage.setItem("todos",JSON.stringify(todos));
+};
 
 
 const patinTodo = (newTodo) =>{
@@ -27,7 +32,9 @@ const handleToDoSubmit = (event) => {
    event.preventDefault();
    const newTodo = toDoInput.value;
    toDoInput.value = "";
+   todos.push(newTodo);
    patinTodo(newTodo);
+   saveToDos();
 }
 
 

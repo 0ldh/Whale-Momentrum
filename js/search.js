4c2ee2ae27d503cb67e56406fc621c7f55fefce4
ldH <ldh19971027@gmail.com>
2022-05-17 15:38:50 +0900

    + serach.js event

--- js/search.js
+++ js/search.js
@@ -3,3 +3,13 @@ const search = searchBox.querySelector("input");
 
 console.log(search);
 console.log(searchBox);
+
+const handleOp = () => {
+    searchBox.style.opacity = "1";
+}
+
+
+search.addEventListener("focus", handleOp)
+search.addEventListener("focusout", ()=> {
+    searchBox.style.opacity = "0.5";
+})

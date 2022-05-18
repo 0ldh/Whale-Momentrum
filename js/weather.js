43936bd2f4fe71f37d62c689cb33285661ff8c49
bb8dd <ldh19971027@gmail.com>
2022-05-18 23:38:03 +0900

    fetch, weather.js

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "cb91bf2e9f480fdb20114b3a09adb6db"
+
+const onGeoOk = (getCurrentPosition) => {
+   console.log(getCurrentPosition)
+   const lat = getCurrentPosition.coords.latitude;
+   const lnt = getCurrentPosition.coords.longitude;
+   console.log(lat, lnt);
+   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lnt}&appid=${API_KEY}&units=metric`
+   fetch(url)
+   .then((response) => response.json())
+   .then((data) => {
+      const name = data.name
+      const weather = data.weather[0].main
+      const tmep = data.main.temp
+   } );
+}
+
+const onGeoError = () => {
+   alert("지억을 찾을 수 없습니다")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)

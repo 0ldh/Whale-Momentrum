dad27b156978fb7f1233d1c470a59fdb17217ccd
ldH <ldh19971027@gmail.com>
2022-05-25 13:37:34 +0900

    greeting 주석

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Whale
-
+> 스타일은 CSS, HTMl은 HTMl, JS는 JS
 
 ## issue
 - 첫 번째 슬라이드에서 두 번째 슬라이드 이동 후 todo check 후 첫 번째 슬라이드에서 다시 두 번째 슬라이드 이동 시 체크해제 => 해결
--- js/greeting.js
+++ js/greeting.js
@@ -1,12 +1,12 @@
 //* slide.js에 import용으로 전부 묶어줌
 const importGreeting = () => {
     //* 변수 생성
-    const loginForm = document.querySelector("#login-form"); // 사용자가 입력할 폼
+    const loginForm = document.querySelector("#login-form"); // 사용자가 입력할 폼 태그
     const loginInput = loginForm.querySelector("#login-form input"); 
     const greeting = document.querySelector("#greeting") // 사용자 이름 그릴 곳
 
-    const HIDDEN_CLASSNAME = "hidden"; // CSS 변경
-    const USERNAME_KEY = "username"; // username 할당 변수
+    const HIDDEN_CLASSNAME = "hidden"; // 클래스명
+    const USERNAME_KEY = "username"; // loaclStorage Key 값
 
     //* username 표시 함수
     const paintGreeting = () => {
@@ -22,31 +22,28 @@ const importGreeting = () => {
         event.preventDefault(); // submit 이벤트의 기본행동을 멈춤 (reload)
         loginForm
             .classList  // 로그인 form의 class List 불러오기
-            .add(HIDDEN_CLASSNAME); // username을 입력하면 form에 .hidden 추가 후 숨김
-        const userName = loginInput.value; // user가 입력한 username 변수에 저장
+            .add(HIDDEN_CLASSNAME); // username을 입력하면 form에 .hidden 추가
+        const userName = loginInput.value; // 유저가 입력한 username 할당
 
         localStorage.setItem(USERNAME_KEY, userName); // username locastorage에 저장
 
         paintGreeting(); // paint greeting 함수 실행
-
-        console.log(userName); //
     }
 
     const savedUsername = localStorage.getItem(USERNAME_KEY); // localstorage에 있는 username을 키로 value 값 호출
 
-    if (savedUsername === null) { // usernmae이 비어있는 경우
+    if (savedUsername === null) { // localsotrage에서 usernmae이 비어있는 경우
         loginForm
             .classList
-            .remove(HIDDEN_CLASSNAME);
-        loginForm.addEventListener("submit", onLoginSubmit)
+            .remove(HIDDEN_CLASSNAME); // loginForm 태그 클래스 리스트 불러와서 클래스명 hidden 삭제
+        loginForm.addEventListener("submit", onLoginSubmit) // loginForm에서 submit 이벤트가 발생 할 경우 onLoginSubmit 함수 실행
     } else { // username이 입력되어 있는 경우
-        paintGreeting();
+        paintGreeting(); // paint greeting 함수 실행 
     }
-
+}
+    // ? .hideen class는 display: none;으로 설정되어 있음
     // ? ClassList => 해당 HTML이 가지고 있는 클래스 목록
     /*  remove(x) : x라는 이름의 클래스명 제거
         add(x) : x라는 이름의 클래스명 추가
         toggle(x) : x라는 이름의 클래스명이 있을 경우 제거 or 추가 */
     // ? preventDefault() =>  기본동작을 막아줌 ? addEventListener => 이벤트를 매개변수로 함수를 실행한다
-
-}

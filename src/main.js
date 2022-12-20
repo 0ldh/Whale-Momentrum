0bb76f80b81b9a947ac26d175b4d07a9960094fa
ldH <ldh19971027@gmail.com>
2022-12-20 14:36:17 +0900

    mainjs 404 error

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import '../dist/BackGround';
-import '../dist/Clock';
-import '../dist/Greeting';
-import '../dist/Quotes';
-import '../dist/Search';
-import '../dist/Slide';
-import '../dist/Todo';
-import '../dist/ViewportSub';

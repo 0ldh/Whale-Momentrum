59178d5f817fbc13587e08f488a9b0aa57bd9ba8
ldH <ldh19971027@gmail.com>
2022-12-21 17:41:50 +0900

    modified Todo

--- src/Todo.ts
+++ src/Todo.ts
@@ -5,18 +5,42 @@ interface Todos {
 }
 
 export default function Todo(element: HTMLDivElement): void {
-  console.log('todo');
+  const ele = element;
   const todoForm = document.createElement('div');
   const todoList = document.createElement('div');
   const todoInput = document.createElement('input');
 
+  todoInput.type = 'Text';
+  /*
+    만들어진 HTML 요소에 속성 추가
+
+  */
+
   todoForm.append(todoList);
   todoForm.append(todoInput);
+  ele.append(todoForm);
 
   const todos:Todos[] = [];
   const todoKey = 'todos';
 
-  const saveTodos = ():void => { localStorage.setItem(todoKey, JSON.stringify(todos)); };
+  const saveTodos = (_newTodo: string):void => {
+    localStorage.setItem(todoKey, JSON.stringify(todos));
+  };
+  const paintTodo = (_todos: Todos[]):void => {
+    _todos.forEach((e) => {
+      const todo = document.createElement('div');
+      const todoText = document.createElement('div');
+      const todoId = document.createElement('div');
+
+      todoText.innerText = e.text;
+      todoId.innerText = String(e.id);
+
+      todo.append(todoText);
+      todo.append(todoId);
+
+      todoList.append(todo);
+    });
+  };
   const addTodo = (event:Event):void => {
     event.preventDefault();
     const newTodo:string = todoInput.value;
@@ -28,8 +52,15 @@ export default function Todo(element: HTMLDivElement): void {
         bool: false,
       };
       todos.push(objectTodo);
+      saveTodos(newTodo);
+      paintTodo(todos);
     }
-
-    saveTodos();
   };
+
+  todoInput.addEventListener('keypress', (e: KeyboardEvent):void => {
+    if (e.key === 'Enter') {
+      console.log('first');
+      addTodo(e);
+    }
+  });
 }

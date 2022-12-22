7524d03b7814f4bd3a6148c59ce8d1c0104a1d65
ldH <ldh19971027@gmail.com>
2022-12-22 09:34:45 +0900

    page1 import Todo

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import setupCounter from './counter';
 import BackGround from './BackGround';
 import Slide from './Slide';
 import Page0 from './Page0';
-import Todo from './Todo';
+import Page1 from './Page1';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div id='wrapper'>
@@ -33,10 +33,4 @@ Slide(
   document.querySelector<HTMLDivElement>('#slide')!,
   document.querySelector<HTMLButtonElement>('#slideBtn')!,
 );
-Todo(document.querySelector<HTMLDivElement>('#page1')!);
-
-/**
- *  * localSotrage에서 불러와서 그려야함 paintTodo(Todo.ts)에 추가
- *  * page1에 Todo 추가 하고 main에서 Todo 지우기
- * first
- */
+Page1(document.querySelector<HTMLDivElement>('#page1')!);
